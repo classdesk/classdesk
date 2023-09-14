72d65eb5a88f8543acd55db8954f2faad9566e39
xnousnow <xnou.snow@gmail.com>
2023-09-14 17:49:02 +0900

    :wrench: `unplugin-icons` 설정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
+<script lang="ts">
+  import Dog from '~icons/lucide/Dog'
+</script>
+
 <h1 class="text-3xl font-bold underline">Hello, world!</h1>
+<Dog class="text-xl" />
 
 <style lang="postcss">
   :global(html) {
