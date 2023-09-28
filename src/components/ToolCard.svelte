1a88fea453d83ec415a5b3303bc989c414f32ff2
xnousnow <xnou.snow@gmail.com>
2023-09-28 11:44:28 +0900

    :construction: 검색 사이드바 UI 완성

---
+++ src/components/ToolCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Star from '~icons/lucide/Star'
+
+  export let Icon: any
+  export let color: string
+  export let name: string
+  export let url: string
+</script>
+
+<li>
+  <a href={url} class="w-full h-24 px-3 pt-1 border-0 btn rounded-xl" style="background: {color}">
+    <div class="flex justify-between w-full">
+      <Icon class="w-6 h-6" />
+      <button>
+        <Star class="w-6 h-6" />
+      </button>
+    </div>
+    <span class="w-full text-lg font-normal text-left">{name}</span>
+  </a>
+</li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
-  import Dog from '~icons/lucide/Dog'
+  import Dices from '~icons/lucide/dices'
+
+  import ToolCard from '../components/ToolCard.svelte'
 </script>
 
-<h1 class="font-mono text-3xl font-bold underline">Hello, world!</h1>
-<p>Inter</p>
-<p class="font-roboto">Roboto</p>
-<Dog class="text-xl" />
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<div class="flex flex-col max-h-screen h-screen p-10 mx-auto max-w-[1080px] 2xl:max-w-[1450px] shrink-0">
+  <nav class="flex items-center justify-between">
+    <h1 class="font-mono text-4xl font-bold">
+      <span class="text-primary">Class</span><span>Desk</span>
+    </h1>
+    <span class="font-mono text-neutral">dev.1</span>
+  </nav>
+  <div class="flex max-h-full py-5">
+    <main class="grow" />
+    <aside class="w-[400px] p-5 pb-0 bg-base-200 flex flex-col max-h-full h-full rounded-2xl gap-5">
+      <input type="text" class="input shrink-0" placeholder="도구 검색" />
+      <div class="max-h-full overflow-y-auto">
+        <ul class="grid grid-cols-2 gap-5">
+          {#each Array(100) as _}
+            <ToolCard Icon={Dices} color="#ffc107" name="도구 이름" url="/" />
+          {/each}
+        </ul>
+      </div>
+    </aside>
+  </div>
+</div>
